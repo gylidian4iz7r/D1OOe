5a0ff7fe8e69f0ddc402ed3ea1289b2c0015c2e0
gylidian4iz7r <wangxia9900@yeah.net>
2021-03-18 09:47:13 +0800

    Initial commit

---
+++ src/main/java/net/shvdy/TestBean.java
@@ -0,0 +1,28 @@
+/**
+ * TestBean
+ * <p>
+ * version 1.0
+ * <p>
+ * 26.02.2020
+ * <p>
+ * Copyright(r) shvdy.net
+ */
+
+package net.shvdy;
+
+public class TestBean {
+
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/net/shvdy/TestSpring.java
@@ -0,0 +1,25 @@
+/**
+ * TestSpring
+ * <p>
+ * version 1.0
+ * <p>
+ * 26.02.2020
+ * <p>
+ * Copyright(r) shvdy.net
+ */
+
+package net.shvdy;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
